aba8752ad9c7aeed4ef1ada9131cd35c51d3913a
Abdullah Ayoola <100965547+abdullah43577@users.noreply.github.com>
2022-11-02 22:31:11 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# Frontend Mentor - Coding bootcamp testimonials slider solution
+
+This is a solution to the [Coding bootcamp testimonials slider challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/coding-bootcamp-testimonials-slider-4FNyLA8JL). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the component depending on their device's screen size
+- Navigate the slider using either their mouse/trackpad or keyboard
+
+### Screenshot
+
+![screenshot of desktop](./design/Screenshot_9.png)
+![screenshot of mobile](./design/Screenshot_12.png)
+
+### Links
+
+- Solution URL: [solution URL here](https://your-solution-url.com)
+- Live Site URL: [live site URL here](https://your-live-site-url.com)
+
+## My process
+
+Desktop-first workflow
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Desktop-first workflow
+
+## Author
+
+- Website - [Abdullah Ayoola](https://www.your-site.com)
+- Frontend Mentor - [@abdullah43577](https://www.frontendmentor.io/profile/abdullah43577)
+- Twitter - [@officialayo540](https://twitter.com/officialayo540)
---
+++ script.js
@@ -0,0 +1,75 @@
+"use strict";
+
+const backward = document.querySelector('[src="./images/icon-prev.svg"]');
+const forward = document.querySelector('[src="./images/icon-next.svg"]');
+const articleSect = document.querySelector("article > h3");
+
+const firstParagraph = document.querySelector(".nameSect :first-child");
+const secondParagraph = document.querySelector(".nameSect :last-child");
+
+// user images
+const userImage = document.querySelector('[src="./images/image-tanya.jpg"]');
+
+const images = ["./images/image-tanya.jpg", "./images/image-john.jpg"];
+
+const article = {
+  0: `“ I’ve been interested in coding for a while but never taken the
+            jump, until now. I couldn’t recommend this course enough. I’m now in
+            the job of my dreams and so excited about the future. ”`,
+
+  1: `“ If you want to lay the best foundation possible I’d recommend taking this course. 
+  The depth the instructors go into is incredible. I now feel so confident about 
+  starting up as a professional developer. ”`,
+};
+
+// console.log(article[0]);
+
+// manipulating the name section and the speciality section
+const fObject = {
+  0: "Tanya Sinclair",
+  1: "John Tarkpor",
+};
+
+const sObject = {
+  0: "UX Engineer",
+  1: "Junior Front-end Developer",
+};
+
+// console.log(article["0"]);
+
+let index = 0;
+
+forward.addEventListener("click", () => {
+  console.log("I'm the forward button");
+  index++;
+  userImage.src = images[index];
+  userImage.alt = "john's image"; // I purposely hardcoded this
+  articleSect.textContent = article[index]; // change the article secion in the DOM
+  firstParagraph.textContent = fObject[index]; // changing the firstParagraph section of the DOM
+  secondParagraph.textContent = sObject[index]; // changing the secondParagraph section of the DOM
+
+  if (index === images.length) {
+    index = 0;
+  }
+  userImage.src = images[index];
+  articleSect.textContent = article[index]; // change the article secion in the DOM
+  firstParagraph.textContent = fObject[index]; // changing the firstParagraph section of the DOM
+  secondParagraph.textContent = sObject[index]; // changing the secondParagraph section of the DOM
+});
+
+backward.addEventListener("click", () => {
+  console.log("I'm the backward button");
+  index--;
+  userImage.src = images[index];
+  articleSect.textContent = article[index]; // change the article secion in the DOM
+  firstParagraph.textContent = fObject[index]; // changing the firstParagraph section of the DOM
+  secondParagraph.textContent = sObject[index]; // changing the secondParagraph section of the DOM
+  console.log(index);
+  if (index < 0) {
+    index = images.length - 1;
+  }
+  userImage.src = images[index];
+  articleSect.textContent = article[index]; // change the article secion in the DOM
+  firstParagraph.textContent = fObject[index]; // changing the firstParagraph section of the DOM
+  secondParagraph.textContent = sObject[index]; // changing the secondParagraph section of the DOM
+});
