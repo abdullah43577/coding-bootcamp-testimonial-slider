a3ef2566c9a3af6e64c81269cc32c324fd73b9a5
Abdullah Ayoola <100965547+abdullah43577@users.noreply.github.com>
2022-11-03 08:48:01 +0100

    added some comments explaining to a friend of mine how I implemented the code

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const secondParagraph = document.querySelector(".nameSect :last-child");
 // user images
 const userImage = document.querySelector('[src="./images/image-tanya.jpg"]');
 
-const images = ["./images/image-tanya.jpg", "./images/image-john.jpg"];
+const images = ["./images/image-tanya.jpg", "./images/image-john.jpg"]; // images.length - 1;
 
 const article = {
   0: `“ I’ve been interested in coding for a while but never taken the
@@ -22,7 +22,7 @@ const article = {
   starting up as a professional developer. ”`,
 };
 
-// console.log(article[0]);
+// console.log(article["0"]);
 
 // manipulating the name section and the speciality section
 const fObject = {
@@ -40,14 +40,18 @@ const sObject = {
 let index = 0;
 
 forward.addEventListener("click", () => {
-  console.log("I'm the forward button");
-  index++;
-  userImage.src = images[index];
+  // forward.addEventlistener('click', function(){})
+  // console.log("I'm the forward button");
+  index++; // index = index + 1;
+  // console.log(index);
+
+  userImage.src = images[index]; // index = 1;
   userImage.alt = "john's image"; // I purposely hardcoded this
   articleSect.textContent = article[index]; // change the article secion in the DOM
   firstParagraph.textContent = fObject[index]; // changing the firstParagraph section of the DOM
   secondParagraph.textContent = sObject[index]; // changing the secondParagraph section of the DOM
 
+  // console.log(index);
   if (index === images.length) {
     index = 0;
   }
@@ -58,15 +62,16 @@ forward.addEventListener("click", () => {
 });
 
 backward.addEventListener("click", () => {
-  console.log("I'm the backward button");
-  index--;
+  // console.log("I'm the backward button");
+  index--; // index = index - 1;
   userImage.src = images[index];
   articleSect.textContent = article[index]; // change the article secion in the DOM
   firstParagraph.textContent = fObject[index]; // changing the firstParagraph section of the DOM
   secondParagraph.textContent = sObject[index]; // changing the secondParagraph section of the DOM
   console.log(index);
+
   if (index < 0) {
-    index = images.length - 1;
+    index = images.length - 1; // index = 1; array are 0 indexed 0, 1
   }
   userImage.src = images[index];
   articleSect.textContent = article[index]; // change the article secion in the DOM
